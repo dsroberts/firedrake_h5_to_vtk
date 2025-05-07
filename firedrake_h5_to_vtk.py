98be428124f67655adf8929d27bf384088cdded6
Dale Roberts <dale.roberts1@anu.edu.au>
2025-05-07 17:31:31 +1000

    Better output handling when run in parallel

--- firedrake_h5_to_vtk.py
+++ firedrake_h5_to_vtk.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import pickle
 import os
+import logging
 from collections import defaultdict
 from enum import Enum
 from petsc4py import PETSc
@@ -57,6 +58,11 @@ class FunctionFilter:
         return self
 
 
+class NullHelpFormatter(argparse.HelpFormatter):
+    def format_help(self):
+        pass
+
+
 def configure_petsc():
     ### Suppress PETSc 'unused options' warning
     opts = PETSc.Options()
@@ -67,12 +73,32 @@ def configure_firedrake():
     pass
 
 
+def configure_logger(verbose: bool, mpi_rank: int) -> logging.Logger:
+    logger = logging.getLogger("h5_to_vtk")
+    logger.setLevel(logging.INFO if verbose else logging.WARNING)
+    if mpi_rank == 0:
+        handler = logging.StreamHandler()
+        handler.setFormatter(fmt=logging.Formatter("%(levelname)s %(message)s"))
+    else:
+        handler = logging.NullHandler()
+    logger.addHandler(handler)
+    return logger
+
+
 def main():
     configure_petsc()
     configure_firedrake()
 
+    ### Make sure help text only appears on a single rank
+    if fd.COMM_WORLD.rank == 0:
+        arg_formatter = argparse.ArgumentDefaultsHelpFormatter
+    else:
+        arg_formatter = NullHelpFormatter
+
     parser = argparse.ArgumentParser(
-        prog="firedrake_h5_to_vtk", description="Convert firedrake HDF5 checkpoint files to VTK output"
+        prog="firedrake_h5_to_vtk",
+        description="Convert firedrake HDF5 checkpoint files to VTK output",
+        formatter_class=arg_formatter,
     )
     parser.add_argument("-i", "--infile", required=True, help="The input firedrake checkpoint file")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -85,24 +111,24 @@ def main():
     )
     parser.add_argument(
         "-f",
-        "--fields",
+        "--functions",
         required=False,
-        help="Comma separeted list of fields to convert. Must match internal G-ADOPT field names. Leave blank for all",
+        help="Comma separeted list of functions to convert. Must match internal function names discovered by firedrake_h5_to_vtk (i.e. output of firedrake_h5_to_vtk -l). Leave blank for all",
         default="all",
     )
     parser.add_argument("-v", "--verbose", help="Be noisy", action="store_true")
 
     ns = parser.parse_args(sys.argv[1:])
-    func_filter = FunctionFilter(ns.fields)
+    logger = configure_logger(ns.verbose, fd.COMM_WORLD.rank)
+
+    func_filter = FunctionFilter(ns.functions)
 
     ### Firedrake will tell us if the checkpoint file isn't a checkpoint file
-    if ns.verbose:
-        print("Attempting to open " + ns.infile)
+    logger.info(f"Attempting to open {ns.infile}")
     checkpoint_file = fd.CheckpointFile(ns.infile, mode="r")
 
     ### Discover meshes
-    if ns.verbose:
-        print(f"Loading global mesh map at {Constants.topologies}/{Constants.global_mesh_map_attr}")
+    logger.info(f"Loading global mesh map at {Constants.topologies}/{Constants.global_mesh_map_attr}")
     topo_dict = pickle.loads(checkpoint_file.h5pyfile[Constants.topologies].attrs[Constants.global_mesh_map_attr])
 
     ### Discover functions
@@ -114,16 +140,14 @@ def main():
     mixed_functions_components = defaultdict(set)
     for mesh_name, topology in topo_dict.items():
         group_path = os.path.join(Constants.topologies, Constants.mixed_meshes_group, mesh_name)
-        if ns.verbose:
-            print(f"Checking for mixed functions on mesh {mesh_name} at path {group_path}")
+        logger.info(f"Checking for mixed functions on mesh {mesh_name} at path {group_path}")
 
         if group_path in checkpoint_file.h5pyfile:
             if Constants.mixed_function_space_attr in checkpoint_file.h5pyfile[group_path].attrs:
                 funcs_from_h5 = pickle.loads(
                     checkpoint_file.h5pyfile[group_path].attrs[Constants.mixed_function_space_attr]
                 )
-                if ns.verbose:
-                    print(f"   Found {[i for i in funcs_from_h5.keys()]}")
+                logger.info(f"   Found {[i for i in funcs_from_h5.keys()]}")
                 mixed_functions[mesh_name].update(funcs_from_h5)
                 ### Discover components of mixed function spaces
                 for func_name, func_desc in funcs_from_h5.items():
@@ -134,8 +158,7 @@ def main():
                         Constants.functions_group,
                         func_name,
                     )
-                    if ns.verbose:
-                        print(f"   Checking for components of {func_name} on {mixed_func_component_path}")
+                    logger.info(f"   Checking for components of {func_name} on {mixed_func_component_path}")
                     if mixed_func_component_path in checkpoint_file.h5pyfile:
                         for sub_func in checkpoint_file.h5pyfile[mixed_func_component_path]:
                             sub_func_group = os.path.join(mixed_func_component_path, sub_func)
@@ -143,29 +166,24 @@ def main():
                                 mixed_functions_components[func_name].add(
                                     checkpoint_file.h5pyfile[sub_func_group].attrs[Constants.functions_attr]
                                 )
-                                if ns.verbose:
-                                    print(
-                                        f"   Found {checkpoint_file.h5pyfile[sub_func_group].attrs[Constants.functions_attr]}"
-                                    )
+                                logger.info(
+                                    f"   Found {checkpoint_file.h5pyfile[sub_func_group].attrs[Constants.functions_attr]}"
+                                )
 
         group_path = os.path.join(Constants.topologies, topology, Constants.meshes_group, mesh_name)
-        if ns.verbose:
-            print(f"Checking for functions on mesh {mesh_name} at path {group_path}")
+        logger.info(f"Checking for functions on mesh {mesh_name} at path {group_path}")
 
         if group_path in checkpoint_file.h5pyfile:
             if Constants.function_space_attr in checkpoint_file.h5pyfile[group_path].attrs:
                 funcs_from_h5 = pickle.loads(checkpoint_file.h5pyfile[group_path].attrs[Constants.function_space_attr])
                 functions[mesh_name].update(funcs_from_h5)
-                if ns.verbose:
-                    print(f"   Found {[i for i in funcs_from_h5.keys()]}")
+                logger.info(f"   Found {[i for i in funcs_from_h5.keys()]}")
 
     ### Filter
     for mesh_name in mixed_functions:
-        if ns.verbose:
-            print(f"Filtering mixed functions on {mesh_name}")
+        logger.info(f"Filtering mixed functions on {mesh_name}")
         mixed_functions[mesh_name] = [func for func in mixed_functions[mesh_name] if func_filter(func)]
-        if ns.verbose:
-            print(f"  Post-filter: {mixed_functions[mesh_name]}")
+        logger.info(f"  Post-filter: {mixed_functions[mesh_name]}")
 
     ### If a mixed function has been requested, we have to ensure the subfunctions get written
     ### regardless of the filter
@@ -175,60 +193,55 @@ def main():
                 func_filter += mixed_functions_components[func]
 
     for mesh_name in functions:
-        if ns.verbose:
-            print(f"Filtering functions on {mesh_name}")
+        logger.info(f"Filtering functions on {mesh_name}")
         functions[mesh_name] = [func for func in functions[mesh_name] if func_filter(func)]
-        if ns.verbose:
-            print(f"  Post-filter: {functions[mesh_name]}")
+        logger.info(f"  Post-filter: {functions[mesh_name]}")
 
     if ns.list:
-        print("Discovered meshes:")
-        for mesh_name in topo_dict.keys():
-            if functions[mesh_name] or mixed_functions[mesh_name]:
-                print(mesh_name)
+        if fd.COMM_WORLD.rank == 0:
+            print("Discovered meshes:")
+            for mesh_name in topo_dict.keys():
+                if functions[mesh_name] or mixed_functions[mesh_name]:
+                    print(mesh_name)
 
-        print("\nDiscovered Functions:")
-        for mesh_name in topo_dict.keys():
-            for f in functions[mesh_name]:
-                print(f" {f} on mesh {mesh_name}")
+            print("\nDiscovered Functions:")
+            for mesh_name in topo_dict.keys():
+                for f in functions[mesh_name]:
+                    print(f" {f} on mesh {mesh_name}")
 
-        print("\nDiscovered Mixed Functions:")
-        for mesh_name in topo_dict.keys():
-            for f in mixed_functions[mesh_name]:
-                print(f" {f} on mesh {mesh_name}")
-                for sub_func in mixed_functions_components[f]:
-                    print(f"  {f} has subfunction {sub_func}")
+            print("\nDiscovered Mixed Functions:")
+            for mesh_name in topo_dict.keys():
+                for f in mixed_functions[mesh_name]:
+                    print(f" {f} on mesh {mesh_name}")
+                    for sub_func in mixed_functions_components[f]:
+                        print(f"  {f} has subfunction {sub_func}")
         return
 
     if not ns.outfile.endswith(".pvd"):
-        print("ERROR: Output file must end in '.pvd'",file=sys.stderr)
+        logger.error("Output file must end in '.pvd'")
         return
 
     ### Load
     meshes_to_load = [mesh_name for mesh_name in topo_dict.keys() if functions[mesh_name] or mixed_functions[mesh_name]]
     ### Warn if we have more than one mesh to load
     if len(meshes_to_load) > 1:
-        print(
-            """WARNING: multiple meshes. PETSc usually crashes when attempting to load multiple meshes in the 
+        logger.warning(
+            """Multiple meshes requested. PETSc usually crashes when attempting to load multiple meshes in the 
             same session. Use the -f option to limit output to functions on a single mesh.
-            """,
-            file=sys.stderr,
+            """
         )
     if len(meshes_to_load) == 0:
-        print("No valid fields selected", file=sys.stderr)
+        logger.warning("No valid functions selected")
         return
 
     functions_to_write = []
     for mesh_name in meshes_to_load:
-        if ns.verbose:
-            print(f"Loading mesh {mesh_name}")
+        logger.info(f"Loading mesh {mesh_name}")
         mesh = checkpoint_file.load_mesh(mesh_name)
         for func in functions[mesh_name]:
-            if ns.verbose:
-                print(f"   Loading function {func}")
+            logger.info(f"   Loading function {func}")
             functions_to_write.append(checkpoint_file.load_function(mesh, func))
-        if ns.verbose:
-            print("Writing VTK output")
+        logger.info("Writing VTK output")
         fd.VTKFile(ns.outfile).write(*functions_to_write)
         del mesh
 
