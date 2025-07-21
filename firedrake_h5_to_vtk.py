64ecb2aebe6f4e39f4592a1a3a1a0d101e37a49d
Dale Roberts <dale.roberts1@anu.edu.au>
2025-07-21 12:01:46 +1000

    Handle timestepping

--- firedrake_h5_to_vtk.py
+++ firedrake_h5_to_vtk.py
@@ -1,4 +1,6 @@
 import firedrake as fd
+import numpy as np
+import numpy.typing as npt
 import sys
 import argparse
 import pickle
@@ -16,6 +18,7 @@ from firedrake.checkpointing import PREFIX, PREFIX_EMBEDDED
 class Constants(str, Enum):
     prefix = PREFIX
     topologies = "topologies"
+    dms = "dms"
     global_mesh_map_attr = prefix + "_mesh_name_topology_name_map"
     function_space_attr = prefix + "_function_name_function_space_name_map"
     mixed_function_space_attr = prefix + "_mixed_function_name_mixed_function_space_name_map"
@@ -56,6 +59,24 @@ class FunctionFilter:
             else:
                 self._filter |= set(other)
         return self
+
+
+class TimestepFilter:
+    def __init__(self, ts_list: str):
+        if ts_list == "all":
+            self.filter = "all"
+        elif ts_list == "last":
+            self.filter = "last"
+        else:
+            self.filter = list(int(i) for i in ts_list.split((",")))
+
+    def __call__(self, timesteps: npt.NDArray[np.float64]) -> list[int]:
+        if self.filter == "all":
+            return [int(i) for i in timesteps]
+        elif self.filter == "last":
+            return [int(timesteps[-1])]
+        else:
+            return [int(i) for i in timesteps if int(i) in self.filter]
 
 
 class NullHelpFormatter(argparse.HelpFormatter):
@@ -113,7 +134,14 @@ def main():
         "-f",
         "--functions",
         required=False,
-        help="Comma separeted list of functions to convert. Must match internal function names discovered by firedrake_h5_to_vtk (i.e. output of firedrake_h5_to_vtk -l). Leave blank for all",
+        help="Comma separeted list of functions to convert. Must match internal function names discovered by firedrake_h5_to_vtk (i.e. output of firedrake_h5_to_vtk -l). Leave blank for all. Ignored when the '-l' option is passed",
+        default="all",
+    )
+    parser.add_argument(
+        "-t",
+        "--timesteps",
+        required=False,
+        help="Comma separated list of timesteps to retrieve from the file. Special values are 'all' for all timesteps and 'last' for the final timestep. Leave blank for all. Ignored when no timestepping is detected in the data",
         default="all",
     )
     parser.add_argument("-v", "--verbose", help="Be noisy", action="store_true")
@@ -121,7 +149,11 @@ def main():
     ns = parser.parse_args(sys.argv[1:])
     logger = configure_logger(ns.verbose, fd.COMM_WORLD.rank)
 
-    func_filter = FunctionFilter(ns.functions)
+    ts_filter = TimestepFilter(ns.timesteps)
+    if ns.list:
+        func_filter = FunctionFilter("all")
+    else:
+        func_filter = FunctionFilter(ns.functions)
 
     ### Firedrake will tell us if the checkpoint file isn't a checkpoint file
     logger.info(f"Attempting to open {ns.infile}")
@@ -136,6 +168,7 @@ def main():
     ### cf['topologies/firedrake_mixed_meshes/{m}'].attrs[PREFIX + '_mixed_function_name_mixed_function_space_name_map'] for m in topo_dict.keys()
     ### cf['topologies/{m[1]}/firedrake_meshes/{m[0]}'].attrs[PREFIX + '_function_name_function_space_name_map'] for m in topo_dict.items()
     functions = defaultdict(dict)
+    timestepping = defaultdict(dict)
     mixed_functions = defaultdict(dict)
     mixed_functions_components = defaultdict(set)
     for mesh_name, topology in topo_dict.items():
@@ -179,10 +212,42 @@ def main():
                 functions[mesh_name].update(funcs_from_h5)
                 logger.info(f"   Found {[i for i in funcs_from_h5.keys()]}")
 
+        group_path = os.path.join(Constants.topologies, topology, Constants.dms)
+        logger.info(f"Checking for timestepping data on mesh {mesh_name} on path {group_path}")
+        ### Mixed functions do not have timestepping data
+        ### If a firedrake_embedded_<function> group exists, the timestepping will be there and not
+        ### in <function>
+        ### Timestepping info will be duplicated between the firedrake_embedded_<function> and <function>
+        ### paths as firedrake_embedded_<function>s are detected separately, however the duplicates will be
+        ### dropped at the filtering stage
+        for func in functions[mesh_name]:
+            logger.info(f"  Checking for {func}")
+            for gpath in checkpoint_file.h5pyfile[group_path]:
+                full_gpath = os.path.join(
+                    group_path, gpath, "vecs", "firedrake_embedded_" + func, "firedrake_timestepping"
+                )
+                if full_gpath in checkpoint_file.h5pyfile:
+                    if "firedrake_timestepping_history_index" in checkpoint_file.h5pyfile[full_gpath].attrs:
+                        timestepping[mesh_name][func] = [
+                            int(i)
+                            for i in checkpoint_file.h5pyfile[full_gpath].attrs["firedrake_timestepping_history_index"]
+                        ]
+                        logger.info(f"    Found embedded timestepping data at {full_gpath}")
+                        continue
+                full_gpath = os.path.join(group_path, gpath, "vecs", func, "firedrake_timestepping")
+                if full_gpath in checkpoint_file.h5pyfile:
+                    if "firedrake_timestepping_history_index" in checkpoint_file.h5pyfile[full_gpath].attrs:
+                        timestepping[mesh_name][func] = [
+                            int(i)
+                            for i in checkpoint_file.h5pyfile[full_gpath].attrs["firedrake_timestepping_history_index"]
+                        ]
+                        logger.info(f"    Found timestepping data at {full_gpath}")
+                        continue
+
     ### Filter
     for mesh_name in mixed_functions:
         logger.info(f"Filtering mixed functions on {mesh_name}")
-        mixed_functions[mesh_name] = [func for func in mixed_functions[mesh_name] if func_filter(func)]
+        mixed_functions[mesh_name] = {func: v for func, v in mixed_functions[mesh_name].items() if func_filter(func)}
         logger.info(f"  Post-filter: {mixed_functions[mesh_name]}")
 
     ### If a mixed function has been requested, we have to ensure the subfunctions get written
@@ -192,9 +257,9 @@ def main():
             if func in mixed_functions_components:
                 func_filter += mixed_functions_components[func]
 
-    for mesh_name in functions:
+    for mesh_name, topology in topo_dict.items():
         logger.info(f"Filtering functions on {mesh_name}")
-        functions[mesh_name] = [func for func in functions[mesh_name] if func_filter(func)]
+        functions[mesh_name] = {func: v for func, v in functions[mesh_name].items() if func_filter(func)}
         logger.info(f"  Post-filter: {functions[mesh_name]}")
 
     if ns.list:
@@ -208,6 +273,8 @@ def main():
             for mesh_name in topo_dict.keys():
                 for f in functions[mesh_name]:
                     print(f" {f} on mesh {mesh_name}")
+                    if f in timestepping[mesh_name]:
+                        print(f"  {f} has timesteps {', '.join([str(i) for i in timestepping[mesh_name][f]])}")
 
             print("\nDiscovered Mixed Functions:")
             for mesh_name in topo_dict.keys():
@@ -215,6 +282,7 @@ def main():
                     print(f" {f} on mesh {mesh_name}")
                     for sub_func in mixed_functions_components[f]:
                         print(f"  {f} has subfunction {sub_func}")
+
         return
 
     if not ns.outfile.endswith(".pvd"):
@@ -235,14 +303,42 @@ def main():
         return
 
     functions_to_write = []
+    output_file = fd.VTKFile(ns.outfile)
+    timestepping_details = {}
+    has_timestepping = False
     for mesh_name in meshes_to_load:
         logger.info(f"Loading mesh {mesh_name}")
         mesh = checkpoint_file.load_mesh(mesh_name)
         for func in functions[mesh_name]:
-            logger.info(f"   Loading function {func}")
-            functions_to_write.append(checkpoint_file.load_function(mesh, func))
-        logger.info("Writing VTK output")
-        fd.VTKFile(ns.outfile).write(*functions_to_write)
+            logger.info(f"   Getting timestepping information for {func}")
+            tsh = checkpoint_file.get_timestepping_history(mesh, func)
+            if tsh:
+                has_timestepping = True
+                logger.info("   Found timestepping information:")
+                timestepping_details[func] = ts_filter(tsh["index"])
+                logger.info(f"     {func} timesteps to write:")
+                logger.info("       " + ", ".join(str(i) for i in timestepping_details[func]))
+            else:
+                logger.info("   No timestepping information")
+
+        all_timesteps = sorted(set(j for i in timestepping_details.values() for j in i))
+        if all_timesteps:
+            for ts in all_timesteps:
+                functions_to_write = []
+                for func in functions[mesh_name]:
+                    if ts in timestepping_details[func]:
+                        functions_to_write.append(checkpoint_file.load_function(mesh, func, idx=ts))
+                if functions_to_write:
+                    logger.info(f"Writing VTK output for timestep {ts}")
+                    output_file.write(*functions_to_write)
+        else:
+            if has_timestepping:
+                logger.error("No valid timesteps selected!")
+                exit(1)
+            for func in functions[mesh_name]:
+                functions_to_write.append(checkpoint_file.load_function(mesh, func))
+            logger.info("Writing VTK output")
+            output_file.write(*functions_to_write)
         del mesh
 
 
